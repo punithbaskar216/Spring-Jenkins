eb730a0d81cbd395d36e9e885263f3a9975188be
unknown <punithbaskar216@gmail.com>
2022-03-28 22:29:23 +0530

    first commit

---
+++ src/test/java/com/springboot/jenkins/JavaTechieSpringBootJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.springboot.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechieSpringBootJenkinsApplicationTests {
+
+	Logger logger = LoggerFactory.getLogger(JavaTechieSpringBootJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case execurting...");
+		assertEquals(true, true);
+	}
+
+}
